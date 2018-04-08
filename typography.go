b0c0972c954e4682f1be8ec3a0772fe0319a5935
Andreas Schröpfer <andreas@apxp.info>
2018-04-08 20:48:37 +0200

    documentation

--- typography.go
+++ typography.go
@@ -2,6 +2,13 @@ package gop5js
 
 import "fmt"
 
+// Text draws the text onto the canvas.
+// The JS Syntax is:
+// text(str,x,y,[x2],[y2])
+// x	Number: x-coordinate of text
+// y	Number: y-coordinate of text
+// x2	Number: by default, the width of the text box, see rectMode() for more info
+// y2	Number: by default, the height of the text box, see rectMode() for more info
 func Text(str string, xyWidth ...float64) {
 	v := joinFloat64(xyWidth...)
 	_, err := sketchDraw.WriteString(
