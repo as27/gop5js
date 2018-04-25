1225cc9ba9a7884066c65cf6cb7918bf08feb9d7
Andreas Schröpfer <andreas@apxp.info>
2018-04-26 00:17:36 +0200

    documentation

--- image.go
+++ image.go
@@ -4,11 +4,22 @@ import "fmt"
 
 // LoadImage preloads the image. When you want to use a image you first
 // have to load it. Then you are able to call Image() with the defined
-// name
+// name. You should not call this function inside the draw() function.
+// The imgFile is the file path inside your file folder. To define that
+// folder you have to set FilesPath
 func LoadImage(imgName, imgFile string) {
 	images[imgName] = imgFile
 }
 
+// Image draws a image to the canvas. Before you can use this function
+// you need to call LoadImage(), because all imges needs to be preloaded.
+//   func init() {
+//   	gop5js.FilesPath = "files"
+//   	gop5js.LoadImage("star", "star.png")
+//   }
+//   func draw() {
+//   	gop5js.Image("star", 10, 10, 30, 30)
+//   }
 func Image(imgName string, x, y, w, h float64) {
 	v := joinFloat64(x, y, w, h)
 
